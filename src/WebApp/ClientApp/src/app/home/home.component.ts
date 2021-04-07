76c5626c2d70ad822ab12e3cacbfa093150bb453
Amael BERTEAU <amael.berteau@tech-easy.fr>
2021-04-07 22:39:54 +0200

    [WebApp/ClientApp] Adding case convert in HomeComponent

--- src/WebApp/ClientApp/src/app/home/home.component.ts
+++ src/WebApp/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { CaseConverterService } from "../services/case-converter.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  inputText: string;
+
+  outputText: string;
+
+  constructor(private caseConverterService: CaseConverterService) {
+
+  }
+
+  convert() {
+    this.caseConverterService.convert(this.inputText).subscribe(result => {
+      this.outputText = result;
+    }, error => console.error(error));
+  }
 }
