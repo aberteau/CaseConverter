18044105d31bbf5fe607d1a4300b1a58791163c8
Amael BERTEAU <amael.berteau@tech-easy.fr>
2021-04-07 22:36:50 +0200

    [WebApp] Adding CaseConverterController

---
+++ src/WebApp/Controllers/CaseConverterController.cs
@@ -0,0 +1,17 @@
+﻿using CaseConverter.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseConverter.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CaseConverterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string str)
+        {
+            string output = CaseConversionHelper.ConvertPascalCaseToSnakeCase(str);
+            return Ok(output);
+        }
+    }
+}
